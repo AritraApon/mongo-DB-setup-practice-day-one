db7c822af678bce01c840675a0d4e4333e76b2cd
AritraApon <aritromazumdar8@gmail.com>
2026-04-23 20:44:50 +0600

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+  <div className="container mx-auto ">
+
+    <h1 className="text-4xl font-bold text-center">hello mongo db
+
+    </h1>
+  </div>
+  );
+}
